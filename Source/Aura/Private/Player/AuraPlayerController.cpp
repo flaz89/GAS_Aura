21170f705f61040a6a67f58f7a4dee36da3eff0d
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2025-08-26 00:34:25 +0200

    - BP: Created BP_AuraPlayerController - C++: completed AuraPlayerController class expanding its logic with input action binding action to it

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,10 +3,11 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
-	bReplicates = true; // it defines if this entity update the server in a networking context
+	bReplicates = true; // it defines if this entity updates the server in a networking context
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -26,4 +27,36 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+/*
+ ************************************** INPUTS
+ */
+
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+/* Callback function for player movement.
+ * Need to get the forward direction of the controller in order to add the correct movement input
+ */
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();	// transform InputActionValue into FVector2D
+	const FRotator Rotation = GetControlRotation();							// extract controller rotation (includes camera rotation)
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);			// created new rotator just with Yaw from Rotation
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X); // extract normalized forward vector 
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);	// extract normalized forward vector 
+
+	if (APawn* ControlledPawn = GetPawn())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);	// cause is linked to W/S key that's why Y
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);	// cause is linked to A/D key that's why X
+	}
 }
