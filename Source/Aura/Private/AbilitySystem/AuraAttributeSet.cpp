517db3e217aef5164e23d12728bcf0c6f1434970
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2025-08-31 09:50:40 +0200

    - C++: added attribute accessor for a simpler attribute management

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -8,6 +8,10 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(50.f);
+	InitMaxMana(50.f);
 }
 
 // thi is the function that always keep networking updated for attribute replication
